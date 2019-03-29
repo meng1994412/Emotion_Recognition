4ce889798101bd4012f25f3cc1483ed2068eb8bf
Sunny Meng <sunnym412@gmail.com>
2019-03-29 15:10:57 -0500

    feat: add the another possible CONV block to the network
    Also fix one previous problem, which is missing one max pooling
    layer on Block #3

--- pipeline/nn/conv/emotionvggnet.py
+++ pipeline/nn/conv/emotionvggnet.py
@@ -8,11 +8,12 @@ from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
 from keras.layers.core import Dropout
+from keras.regularizers import l2
 from keras import backend as K
 
 class EmotionVGGNet:
     @staticmethod
-    def build(width, height, depth, classes):
+    def build(width, height, depth, classes, reg = 0.0005):
         # initialize the model along with the input shape to be
         # "channels last" and the channels dimension itself
         model = Sequential()
@@ -26,10 +27,11 @@ class EmotionVGGNet:
 
         # Block #1: first CONV => ELU => CONV => ELU => POOL layer set
         model.add(Conv2D(32, (3, 3), padding = "same",
-            kernel_initializer = "he_normal", input_shape = inputShape))
+            kernel_initializer = "he_normal", kernel_regularizer = l2(reg),
+            input_shape = inputShape))
         model.add(ELU())
         model.add(BatchNormalization(axis = chanDim))
-        model.add(Conv2D(32, (3, 3), padding = "same",
+        model.add(Conv2D(32, (3, 3), padding = "same", kernel_regularizer = l2(reg),
             kernel_initializer = "he_normal"))
         model.add(ELU())
         model.add(BatchNormalization(axis = chanDim))
@@ -37,11 +39,11 @@ class EmotionVGGNet:
         model.add(Dropout(0.25))
 
         # Block #2: second CONV => ELU => CONV => ELU => POOL layer set
-        model.add(Conv2D(64, (3, 3), padding = "same",
+        model.add(Conv2D(64, (3, 3), padding = "same", kernel_regularizer = l2(reg),
             kernel_initializer = "he_normal"))
         model.add(ELU())
         model.add(BatchNormalization(axis = chanDim))
-        model.add(Conv2D(64, (3, 3), padding = "same",
+        model.add(Conv2D(64, (3, 3), padding = "same", kernel_regularizer = l2(reg),
             kernel_initializer = "he_normal"))
         model.add(ELU())
         model.add(BatchNormalization(axis = chanDim))
@@ -49,31 +51,48 @@ class EmotionVGGNet:
         model.add(Dropout(0.25))
 
         # Block #3: third CONV => ELU => CONV => ELU => POOL
-        model.add(Conv2D(128, (3, 3), padding = "same",
+        model.add(Conv2D(128, (3, 3), padding = "same", kernel_regularizer = l2(reg),
             kernel_initializer = "he_normal"))
         model.add(ELU())
         model.add(BatchNormalization(axis = chanDim))
-        model.add(Conv2D(128, (3, 3), padding = "same",
+        model.add(Conv2D(128, (3, 3), padding = "same", kernel_regularizer = l2(reg),
             kernel_initializer = "he_normal"))
         model.add(ELU())
         model.add(BatchNormalization(axis = chanDim))
+        model.add(MaxPooling2D(pool_size = (2, 2)))
+        model.add(Dropout(0.25))
+
+        # Block #3*: fourth CONV => ELU => CONV => ELU => POOL
+        # uncomment this block for experiment #5 or later
+        model.add(Conv2D(256, (3, 3), padding = "same", kernel_regularizer = l2(reg),
+            kernel_initializer = "he_normal"))
+        model.add(ELU())
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(Conv2D(256, (3, 3), padding = "same", kernel_regularizer = l2(reg),
+            kernel_initializer = "he_normal"))
+        model.add(ELU())
+        model.add(BatchNormalization(axis = chanDim))
+        model.add(MaxPooling2D(pool_size = (2, 2)))
         model.add(Dropout(0.25))
 
         # Block #4: first set of FC => ELU layer set
         model.add(Flatten())
-        model.add(Dense(64, kernel_initializer = "he_normal"))
+        model.add(Dense(64, kernel_initializer = "he_normal",
+            kernel_regularizer = l2(reg)))
         model.add(ELU())
         model.add(BatchNormalization(axis = chanDim))
         model.add(Dropout(0.5))
 
         # Block #5: second set of FC => ELU layer set
-        model.add(Dense(64, kernel_initializer = "he_normal"))
+        model.add(Dense(64, kernel_initializer = "he_normal",
+            kernel_regularizer = l2(reg)))
         model.add(ELU())
         model.add(BatchNormalization(axis = chanDim))
         model.add(Dropout(0.5))
 
         # Block #6: softmax classifier
-        model.add(Dense(classes, kernel_initializer = "he_normal"))
+        model.add(Dense(classes, kernel_initializer = "he_normal",
+            kernel_regularizer = l2(reg)))
         model.add(Activation("softmax"))
 
         # return the constructed network architecture
